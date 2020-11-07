fcdf986af3b30d1ec6c6a96aebd0ecd46099c9f5
atbeesley <alexander.beesley@gmail.com>
2020-11-07 13:47:13 +0000

    gets json response when browser hits  /articles

--- main.go
+++ main.go
@@ -4,7 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request){
+	articles := Articles{
+		Article{Title:"Test Title", Desc:"Test description", Content:"test content"},
+	}
+
+	fmt.Println("Endpoint hit: all articles endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "Homepage endpoint hit")
@@ -12,6 +30,7 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func handleRequests(){
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
